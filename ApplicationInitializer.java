b35339f1c04efd9359853ce871d57a91b3df5755
togobikakrishnan <togobikakrishnan@gmail.com>
2025-11-03 14:46:47 +0530

    Add files via upload

---
+++ ApplicationInitializer.java
@@ -0,0 +1,17 @@
+package cams.inv.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gobika g 
+ * Main class for Inventory Management System
+ */
+@SpringBootApplication
+public class ApplicationInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationInitializer.class, args);
+	}
+
+}
